e9ac8cce5a43269bb1323fca06dc747b48871f90
Teddy Martin <tkmartin@smith.edu>
2023-03-28 13:10:16 -0400

    started house class by adding methods

--- House.java
+++ House.java
@@ -1,8 +1,53 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+/* This is a stub for the House class */
+public class House extends Building {
+  private ArrayList<String> residents; 
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, ArrayList<String> residents, boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
+  }
+
+  /** dining room accessor */
+  public boolean hasDiningRoom() {
+    return hasDiningRoom();
+  }
+
+  /** accessor of number of residents */
+  public int nResidents() {
+    return residents.size();
+  }
+
+  /**  */
+  public void moveIn(String name) {
+    if (this.residents.contains(name)) { // already in building
+      throw new RuntimeException(name + " is already onboard " + this.name);
+    }
+    this.residents.add(name); // successfully moved in
+  System.out.println(name + " successfully moved into " + this.name); 
+  }
+  
+  public String moveOut(String name) {
+    if ((!this.residents.contains(name))) { // not resident of building
+      throw new RuntimeException(name + " is not a resident of " + this.name);
+  }
+  this.residents.remove(name); // successfully moves out
+  return name;
+  }
+
+  /** returns boolean of whether or not someone is a resident of a house
+   * @param person name of resident to be checked
+   */
+  public boolean isResident(String person) {
+    if ((this.residents.contains(person))) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public static void main(String[] args) {
