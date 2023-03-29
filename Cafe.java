88591fbb49448e5de71cdc26c4e47fa1fef0050a
Teddy Martin <tkmartin@smith.edu>
2023-03-29 12:01:41 -0400

    added restock functionality to cafe

--- Cafe.java
+++ Cafe.java
@@ -5,6 +5,16 @@ public class Cafe extends Building {
     private int nCreams; // The number of "splashes" of cream remaining in inventory
     private int nCups; // The number of cups remaining in inventory
 
+    /**
+     * Cafe constructor
+     * @param name
+     * @param address
+     * @param nFloors
+     * @param nCoffeeOunces
+     * @param nSugarPackets
+     * @param nCreams
+     * @param nCups
+     */
     public Cafe(String name, String address, int nFloors, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
         super(name, address, nFloors);
         this.nCoffeeOunces = nCoffeeOunces;
@@ -14,19 +24,50 @@ public class Cafe extends Building {
         System.out.println("You have built a cafe: ☕");
     }
 
+    /**
+     * sells coffee and reduces nCups by 1, and other variables by user specifications in the parameters
+     * @param size
+     * @param nSugarPackets
+     * @param nCreams
+     */
     public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        if (size > this.nCoffeeOunces) {
+            this.restock(50, 0, 0, 0);
+        }
+        if (nSugarPackets > this.nSugarPackets) {
+            this.restock(0, 50, 0, 0);
+        }
+        if (nCreams > this.nCreams) {
+            this.restock(0, 0, 25, 0);
+        }
+        if (this.nCups < 1) {
+            this.restock(0, 0, 0, 100);
+        }
         this.nCoffeeOunces = this.nCoffeeOunces - size;
         this.nSugarPackets = this.nSugarPackets - nSugarPackets;
         this.nCreams = this.nCreams - nCreams;
+        this.nCups = this.nCups - 1;
         System.out.println("You sold a coffee with the size of " + size + ", " + nSugarPackets + " sugar packet(s), " + "and " + nCreams + " cream(s).");
     }
 
+    /**
+     * prints out inventory including number of cups, coffee ounces, sugar packets, and creams.
+     */
     public void printInventory() {
-        System.out.println(this.name + " currently has " + this.nCoffeeOunces + " cups, " + this.nSugarPackets + " sugar packet(s), " + "and " + this.nCreams + " cream(s).");
+        System.out.println(this.name + " currently has " + this.nCups + " cup(s), " + this.nCoffeeOunces + " coffee ounce(s), " + this.nSugarPackets + " sugar packet(s), " + "and " + this.nCreams + " cream(s) in its inventory.");
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        this.nCoffeeOunces = this.nCoffeeOunces + nCoffeeOunces;
+        this.nSugarPackets = this.nSugarPackets + nSugarPackets;
+        this.nCreams = this.nCreams + nCreams;
+        this.nCups = this.nCups + nCups;
+        System.out.println("The inventory has been updated.");
+        this.printInventory();
     }
     
     public static void main(String[] args) {
-        Cafe Ediya = new Cafe("Ediya Coffee", "5 Mapogu Way", 1, 30, 20, 10, 10);
+        Cafe Ediya = new Cafe("Ediya Coffee", "5 Mapogu Way", 1, 30, 20, 10, 0);
         Ediya.sellCoffee(3, 1, 1);
         Ediya.printInventory();
     }
